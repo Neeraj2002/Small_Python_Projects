6b15c03782faeddb4f2454f69da89c4259469be7
Neeraj Rathod <75560703+Neeraj2002@users.noreply.github.com>
2021-10-16 22:05:36 +0530

    PyGame: Car Game

--- PyGame/Car Gme/main.py
+++ PyGame/Car Gme/main.py
@@ -7,7 +7,10 @@ from utils import scale_image, blit_rotate_center
 GRASS = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/track1.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/track-border1.png"), 0.9)
-FINISH = pygame.image.load("SmallProjects/PyGame/Car Game/assets/finish.png")
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+FINISH = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/finish.png"), 0.8)
+FINISH_POSITION = (90, 220)
+FINISH_MASK = pygame.mask.from_surface(FINISH)
 
 RED_CAR = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/red-car.png"), 0.4)
 GREEN_CAR = scale_image(pygame.image.load("SmallProjects/PyGame/Car Game/assets/green-car.png"), 0.4)
@@ -44,6 +47,10 @@ class AbstractCar:
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
 
+    def move_backward(self):
+        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
+        self.move()
+
     def move(self):
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
@@ -56,11 +63,35 @@ class AbstractCar:
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
 
+    def boost(self):
+        self.vel = max(self.vel - self.acceleration / 5, 0)
+        self.move()
+
+    def slow_mo(self):
+        self.vel -= 1
+        self.vel = 0.5
+        self.move()
+
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+
 
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
-    START_POS = (130, 200)
+    START_POS = (130, 180)
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
+
+    def reset(self):
+        self.x,self.y = self.START_POS
+        self.angle = 0
+        self.vel = 0
 
 
 def draw(win, images, player_car):
@@ -70,10 +101,35 @@ def draw(win, images, player_car):
     player_car.draw(win)
     pygame.display.update()
 
+
+def move_player(player_car):
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
+    if keys[pygame.K_LSHIFT]:
+        moved = True
+        player_car.boost()
+    if keys[pygame.K_LCTRL]:
+        moved = True
+        player_car.slow_mo()
+
+    if not moved:
+        player_car.reduce_speed()
+
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
-player_car = PlayerCar(4, 4)
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
+player_car = PlayerCar(8, 8)
 
 
 while run:
@@ -87,18 +143,22 @@ while run:
             run = False
             break
 
-    keys = pygame.key.get_pressed()
-    moved = False
+    move_player(player_car)
 
-    if keys[pygame.K_a]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_d]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_w]:
-        moved = True
-        player_car.move_forward()
-    if not moved:
-        player_car.reduce_speed()
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
+
+    finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
+    if finish_poi_collide != None:
+        if finish_poi_collide[1] == 0:
+            player_car.bounce()
+        else:
+            player_car.reset()
+            print("Finish")
+
+
+
+
 
 
 pygame.quit()
